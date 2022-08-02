0162142149bc35229cce4bd4fc40748a2a092f31
Krasskoo <krasimirbogdanov99@gmail.com>
2022-08-02 16:23:22 +0300

    Testing some new changes, but still problem here. Maybe variable is the problem.

--- Anagram/Form1.cs
+++ Anagram/Form1.cs
@@ -14,7 +14,6 @@ namespace Anagram
             String [] wordList = System.IO.File.ReadAllText(@"C:\Users\Krasskoo\Desktop\Untitled.txt").Split();
             String inputString = "documenting";
 
-            
 
             char[] wrdArray1 = inputString.ToUpper().ToCharArray();
             Array.Sort(wrdArray1);
@@ -27,12 +26,21 @@ namespace Anagram
                 String newWord2 = new string(wrdArray2);
                 if (newWord2 == newWord1)
                 {
-                    string matchedWord = wordList[i];
-                    string[] matchedWords = new string[] { wordList[i] }; // error here!
-                    string resultWords = string.Join(",", matchedWords);
-                    richTextBox2.Text = resultWords;
-                    MessageBox.Show(resultWords);
+                    string matchedWord = wordList[i]; // MAYBE PROBLEM HERE ?
+                    /* 
+                      string[] matchedWords = new string[] { matchedWord }; // maybe problem here? variable is overwritten and the last written replace already stored variable in array
+                      List<string> list = new List<string>(matchedWords.ToList());
+                      list.Add(matchedWord);
+                      matchedWords = list.ToArray();
+                      string resultWords = string.Join(", ", matchedWords);
+                      richTextBox2.Text = list.ToString();
+                    */
+                    List<string> matchedWordsListFinal = new List<string>();
+                    matchedWordsListFinal.Add(matchedWord);
+                    richTextBox2.Lines = matchedWordsListFinal.ToArray();
+                    MessageBox.Show(matchedWord);
                 }
+
             }
         }
     }
