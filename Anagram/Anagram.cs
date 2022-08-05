8efa71aeab43a6d455032c788c27825c11cc51a9
Krasskoo <krasimirbogdanov99@gmail.com>
2022-08-05 19:26:59 +0300

    Changed maybe the entire program, because new algorithm. Now check one word with every word in the list. Need some checks of code.

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -1,4 +1,4 @@
-
+﻿
 namespace Anagram
 {
     public partial class Anagram : Form
@@ -24,8 +24,60 @@ namespace Anagram
                 char[] wrdArray1 = inputString.ToUpper().ToCharArray();
                 Array.Sort(wrdArray1);
                 String newWord1 = new string(wrdArray1);
+                
+                for (int m=0; m<wordCount.Length - 1; m++)
+                {
+                    int inputStringLength = inputString.Length; // Дължина на въведения стринг
+                  //  MessageBox.Show("input " + inputString + " dulug: " + inputStringLength);
+                    int matchingWord = wordList[m].Length; // Дължина на m стринг от списъка (първия)
+                    if (inputStringLength == matchingWord) //Ако думата е еднакво дълга с въведената 
+                    {
+                        char[] matchingWordArr = wordList[m].ToUpper().ToCharArray();
+                        Array.Sort(matchingWordArr);
+                        String matchingNewWord = new String(matchingWordArr);
+                        if (newWord1 == matchingNewWord) // Ако символите са еднакви
+                        {
+                            matchedWordsListFinal.Add(wordList[m]); //Запис
+                    //        MessageBox.Show("Zapis " + wordList[m]);
+                            richTextBox2.Lines = matchedWordsListFinal.ToArray();
+                        }
+                    }
+                    // ----------------WORKS VERY GOOD----------------------
+                     else
+                     {
+                     //    MessageBox.Show("matchingWord " + wordList[m] + " /" + matchingWord);
+                         for (int n = 0; n < wordCount.Length - 1; n++)
+                         {
+                             int nextWord = wordList[n].Length; // Дължина на следващият стринг 
+                      //       MessageBox.Show("nextWord " + wordList[n] + " /" + nextWord);
+                             int matchingWordConcat = matchingWord + nextWord; // Дължина на два поредни стринга
+                        //     MessageBox.Show("concat " + matchingWordConcat);
 
-                for (int j = 0; j < wordCount.Length; j++)
+                             if (inputStringLength == matchingWordConcat)
+                             {
+                                 char[] wrdArrayFirstCheck = wordList[m].ToUpper().ToCharArray();
+                                 char[] wrdArrayLastCheck = wordList[n].ToUpper().ToCharArray();
+                                 List<char> list = new List<char>(wrdArrayFirstCheck.Length + wrdArrayLastCheck.Length);
+                                 list.AddRange(wrdArrayFirstCheck);
+                                 list.AddRange(wrdArrayLastCheck);
+                                 list.ToArray();
+                                 char[] concated = list.ToArray();
+                                 Array.Sort(concated);
+                                 String concatenatedWord = new string(concated);
+
+                                 if (newWord1 == concatenatedWord)
+                                 {
+                                     matchedWordsListFinal.Add(wordList[m] + " + " + wordList[n]);
+                                     //matchedWordsListFinal.Add(wordList[n]);
+                         //            MessageBox.Show("Zapis v cikula 1va duma " + wordList[m] + " 2ra duma " + wordList[n]);
+                                     richTextBox2.Lines = matchedWordsListFinal.ToArray();
+                                }  
+                             }
+                         }
+                     }
+                }
+
+             /*   for (int j = 0; j < wordCount.Length; j++)
                 {
                     char[] wrdArrayFirstCheck = wordList[j].ToUpper().ToCharArray();
                     Array.Sort(wrdArrayFirstCheck);
@@ -36,31 +88,32 @@ namespace Anagram
                         matchedWordsListFinal.Add(matchedWord);
                     }
                 }
-                for (int i = 0; i < wordCount.Length; i++)
-                {
-                    char[] wrdArray2 = wordList[i].ToUpper().ToCharArray();
-                    char[] wrdArray3 = wordList[i+1].ToUpper().ToCharArray();
-                    List<char> list = new List<char>(wrdArray2.Length + wrdArray3.Length);
-                    list.AddRange(wrdArray2);
-                    list.AddRange(wrdArray3);
-                    list.ToArray();
-                    char[] concated = list.ToArray();
-                    Array.Sort(concated);
-                    String newWord2 = new string(concated);
+                 for (int i = 0; i < wordCount.Length; i++)
+                 {
+                     char[] wrdArray2 = wordList[i].ToUpper().ToCharArray();
+                     char[] wrdArray3 = wordList[i+1].ToUpper().ToCharArray();
+                     List<char> list = new List<char>(wrdArray2.Length + wrdArray3.Length);
+                     list.AddRange(wrdArray2);
+                     list.AddRange(wrdArray3);
+                     list.ToArray();
+                     char[] concated = list.ToArray();
+                     Array.Sort(concated);
+                     String newWord2 = new string(concated);
 
-                    if (newWord2 == newWord1)
-                    {
-                        string matchedWord1 = wordList[i];
-                        string matchedWord2 = wordList[i+1];
-                        matchedWordsListFinal.Add(matchedWord1);
-                        matchedWordsListFinal.Add(matchedWord2);
-                    }
-                    richTextBox2.Lines = matchedWordsListFinal.ToArray();
-                    if (matchedWordsListFinal.Count == 0)
-                    {
-                        richTextBox2.Text = "There are not Anagrams with that word!";
-                    }
-                }
+                     if (newWord2 == newWord1)
+                     {
+                         string matchedWord1 = wordList[i];
+                         string matchedWord2 = wordList[i+1];
+                         matchedWordsListFinal.Add(matchedWord1);
+                         matchedWordsListFinal.Add(matchedWord2);
+                     }
+
+                     if (matchedWordsListFinal.Count == 0)
+                     {
+                         richTextBox2.Text = "There are not Anagrams with that word!";
+                     }
+                 }*/
+              //  richTextBox2.Lines = matchedWordsListFinal.ToArray();
             }
         }
     }
